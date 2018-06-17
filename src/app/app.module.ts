017aca81034ef4d46b208497e2fc2f7b4220ddf0
RAHUL AGARWAL <rahul89.agarwal@gmail.com>
2018-06-18 01:24:17 +0530

    removed unused nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,12 @@ import { RoutingRoutes } from './routing.routing';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { RestClientService } from './rest-client.service';
-import { PublicNavComponent } from './public-nav/public-nav.component';
-import { SecureNavComponent } from './secure-nav/secure-nav.component';
 import { WorkoutComponent } from './workout/workout.component';
 import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PublicNavComponent,
-    SecureNavComponent,
     SigninComponent,
     SignupComponent,
     WorkoutComponent,
