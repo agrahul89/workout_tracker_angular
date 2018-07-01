7e9e31be6a8fd7a932744a1ba633da1d87da8f47
RAHUL AGARWAL <rahul89.agarwal@gmail.com>
2018-07-01 21:06:51 +0530

    added auth guard implementation to router

--- src/app/routing.routing.ts
+++ src/app/routing.routing.ts
@@ -1,20 +1,20 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthService } from './_services/auth.service';
 import { AppComponent } from './app.component';
 import { CategoryComponent } from './category/category.component';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { WorkoutComponent } from './workout/workout.component';
+import { AuthGuardService } from './_services/auth-guard.service';
 
 const routes: Routes = [
   // TODO Implement Authentication/Routing Guard
-  {path: '', pathMatch: 'full', redirectTo: 'signin'/* , canActivate: [AuthService] */},
+  {path: '', pathMatch: 'full', redirectTo: 'signin'},
   {path: 'signin',   component: SigninComponent},
   {path: 'signup',   component: SignupComponent},
   {path: 'logout',   component: AppComponent},
-  {path: 'workout',  component: WorkoutComponent},
-  {path: 'category', component: CategoryComponent},
+  {path: 'category', component: CategoryComponent, canActivate: [AuthGuardService]},
+  {path: 'workout',  component: WorkoutComponent,  canActivate: [AuthGuardService]},
 ];
 
 export const RoutingRoutes = RouterModule.forRoot(routes);
